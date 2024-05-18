d74970fcb3b725fc6aaa1531128610f7fa1ee6f1
Said-ob7 <saideloubid2@gmail.com>
2024-05-18 14:13:12 +0100

    initial commit

---
+++ src/main/java/com/emsi/ebankbackend/digital_bancking_backendApplication.java
@@ -0,0 +1,13 @@
+package com.emsi.ebankbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class digital_bancking_backendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(digital_bancking_backendApplication.class, args);
+    }
+
+}
